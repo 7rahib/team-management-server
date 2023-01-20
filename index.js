ed2d4cb6f46e5c4e3993e22aa99259dd63453b48
7rahib <rahibrashid002@gmail.com>
2023-01-20 23:43:42 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const ObjectId = require('mongodb').ObjectId;
+
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wzwnadc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+async function run() {
+    try {
+        await client.connect()
+        const usersCollection = client.db('team-management').collection('users');
+
+        // Storing user data in database
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24d' });
+            res.send({ result, token });
+        })
+
+
+        app.get('/users', verifyJWT, async (req, res) => {
+            const yourOrder = await usersCollection.find().toArray()
+            res.send(yourOrder)
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
+// ROOT API
+app.get('/', (req, res) => {
+    res.send('Team Management server is running')
+})
+
+app.listen(port, () => {
+    console.log('Listening to ', port)
+})
